1f92e3a2f5c1a70195fe16b4b2742541e365fa8c
Mario V <109741407+mariotfglabs@users.noreply.github.com>
2022-10-03 19:05:08 +0200

    Add files via upload

---
+++ dbt-snow-mask/macros/snow-mask/apply-policy/apply_masking_policy.sql
@@ -0,0 +1,13 @@
+{% macro apply_masking_policy(resource_type="models",meta_key="masking_policy") %}
+
+    {% if execute %}
+
+        {% if resource_type == "sources" %}
+            {{ dbt_snow_mask.apply_masking_policy_list_for_sources(meta_key) }}
+        {% elif resource_type == "models" %}
+            {{ dbt_snow_mask.apply_masking_policy_list_for_models(meta_key) }}
+        {% endif %}
+
+    {% endif %}
+
+{% endmacro %}
